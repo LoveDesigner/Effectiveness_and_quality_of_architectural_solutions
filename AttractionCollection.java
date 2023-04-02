bca50af23400b191583cb237ed6f21fbe7cda4c5
Love Kurbatova <64596732+LoveDesigner@users.noreply.github.com>
2023-04-02 15:32:31 +0300

    Delete AttractionCollection.java

--- AttractionCollection.java
+++
@@ -1,9 +0,0 @@
-public interface AttractionCollection {
-
-	public void addAttraction(Attraction attraction);
-	
-	public void removeAttraction(Attraction attraction);
-	
-	public AttractionIterator iterator(TravelTypeEnum type);
-	
-}
